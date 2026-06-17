8e292059a14cbdff13f14fe92a1c63a5e9bb0c69
rahmidwintan09 <rahmidwintan@gmail.com>
2026-06-17 13:51:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def about_page():
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.image("https://github.com/rahmidwintan09/CoVision/blob/212064132008d93a15a9c74e975bbd1e2977903f/images/matang.jpg", caption="Matang", use_container_width=True)
+        st.image("https://raw.githubusercontent.com/rahmidwintan09/CoVision/212064132008d93a15a9c74e975bbd1e2977903f/images/matang.jpg", caption="Matang", use_container_width=True)
         st.markdown("""
         **Matang (Grade A)**  
         - Warna merah merata  
@@ -112,7 +112,7 @@ def about_page():
         """)
 
     with col2:
-        st.image("https://github.com/rahmidwintan09/CoVision/blob/212064132008d93a15a9c74e975bbd1e2977903f/images/setengah_matang.jpg", caption="Setengah Matang", use_container_width=True)
+        st.image("https://raw.githubusercontent.com/rahmidwintan09/CoVision/212064132008d93a15a9c74e975bbd1e2977903f/images/setengah_matang.jpg", caption="Setengah Matang", use_container_width=True)
         st.markdown("""
         **Setengah Matang (Grade B)**  
         - Warna kuning  
@@ -121,7 +121,7 @@ def about_page():
         """)
 
     with col3:
-        st.image("https://github.com/rahmidwintan09/CoVision/blob/212064132008d93a15a9c74e975bbd1e2977903f/images/mentah.jpg", caption="Mentah", use_container_width=True)
+        st.image("https://raw.githubusercontent.com/rahmidwintan09/CoVision/212064132008d93a15a9c74e975bbd1e2977903f/images/mentah.jpg", caption="Mentah", use_container_width=True)
         st.markdown("""
         **Mentah (Grade C)**  
         - Warna hijau 
