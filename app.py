3d1e855669c60a47b8afc8bb9dea9fb3006b0484
rahmidwintan09 <rahmidwintan@gmail.com>
2026-07-21 22:11:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import sys
 import os
 os.environ["OPENCV_HEADLESS"] = "1"
 os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
-os.environ["OPENCV_VIDEOIO_PRIORITY_MSMF"] = "0"
 os.environ["OPENCV_VIDEOIO_PRIORITY_BACKEND"] = "0"
 
 import streamlit as st    
